21221157e11b4548851ccede8e89d1c8a0d8a85d
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2024-05-12 03:52:12 -0300

    hide_env

--- src/pages/Home/sections/Contato/Contato.jsx
+++ src/pages/Home/sections/Contato/Contato.jsx
@@ -1,5 +1,4 @@
 import './Contato.css'
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Star2 from '../../../../assets/images/star2.png'
@@ -8,22 +7,23 @@ import Intro from '../../../../components/Intro/Intro'
 import Inbox from '../../../../components/Inbox/Inbox'
 
 const Contato = () => {
-    const whatsappNumber = '+5583986727988'
+    const phone = process.env.REACT_APP_WHATSAPP
+    const email = process.env.REACT_APP_EMAIL
 
     return (
         <section id='contato'>
             <img src={Star2} alt="" className='illustration' />
             <Intro medium={true} title='Contato' text='Sinta-se à vontade para entrar em contato comigo através dos métodos abaixo e retornarei em contato com você o mais breve possível.' />
             <div id="inbox">
-                <Link className='inbox-link' to='mailto:nanvieiradev@gmail.com' target='_blank' rel='noopener noreferrer'>
+                <Link className='inbox-link' to={`mailto:${email}`} target='_blank' rel='noopener noreferrer'>
                     <Inbox text='E-mail' />
                 </Link>
-                <Link className='inbox-link' to={`https://wa.me/${whatsappNumber}`} target='_blank' rel='noopner noreferrer'>
+                <Link className='inbox-link' to={`https://wa.me/${phone}`} target='_blank' rel='noopner noreferrer'>
                     <Inbox image={''} text='WhatsApp' />
                 </Link>
             </div>
             <img src={Helix} alt="" className='illustration' />
-        </section>
+        </section >
     )
 }
 
