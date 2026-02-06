b5f4f4743f291423d35a072d46e1d959bcf568ce
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-06 16:04:40 -0300

    chore: estrutura inicial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: "#6366f1",
+          700: "#4338ca",
+        },
+        secondary: "#22c55e",
+        danger: "#ef4444",
+      },
+      borderRadius: {
+        xl: "1rem",
+      },
+    },
+  },
+};
