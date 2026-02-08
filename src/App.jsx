bcac732ada119da347b9e74ee88675a077291fe7
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-08 19:38:02 -0300

    Style: padronização do modo escuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from '@/components/theme-provider'
+
 function App() {
-  return <></>
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <button className="bg-primary rounded-lg text-primary-foreground">
+        Comprar
+      </button>
+    </ThemeProvider>
+  )
 }
 
 export default App
